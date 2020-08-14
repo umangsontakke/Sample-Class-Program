f3b231fbff0ecd8bd30f98f224c77361c615d28d
umangsontakke <50995664+umangsontakke@users.noreply.github.com>
2020-08-14 14:22:59 +0530

    Update SampleClassProgram.java

--- SampleClassProgram.java
+++ SampleClassProgram.java
@@ -1,32 +1,37 @@
 public class Charge
-{
-private final double rx,ry;
-private final double q;
+	{
+	//instance variable
+		private final double rx,ry;
+		private final double q;
 
+	//method declaration
 public Charge(double x0, double y0, double q0)
-{
-rx=x0; ry=y0;q=q0;
-}
+	{
+		rx=x0; ry=y0;q=q0;
+	}
+	//method declaration
 public double potentialAt(double x, double y)
-{
-double k=8.99e09;
-double dx=x - rx;
-double dy=y - ry;
-return k * q / Math.sqrt(dx*dx + dy*dy);
-}
+	{
+		double k=8.99e09;
+		double dx=x - rx;
+		double dy=y - ry;
+		return k * q / Math.sqrt(dx*dx + dy*dy);
+	}
 
 public String toString()
-{
-return q+"at"+"("+ rx +","+ ry +")";}
+	{
+		return q+"at"+"("+ rx +","+ ry +")";
+	}
 
+	//main method
 public static void main(String[] args)
-{
-	double x=Double.parseDouble(args[0]);
-	double y=Double.parseDouble(args[1]);
-	Charge.c1=new Charge(0.51, 0.63, 21.3);
-	Charge.c2=new Charge(0.13, 0.94, 81.9);
-	double v1=c1.potentialAt(x, y);
-	double v2=c2.potentialAt(x, y);
-	StdOut.printf("%.2e\n",  (v1 + v2));
-}
+	{
+		double x=Double.parseDouble(args[0]);
+		double y=Double.parseDouble(args[1]);
+		Charge.c1=new Charge(0.51, 0.63, 21.3);
+		Charge.c2=new Charge(0.13, 0.94, 81.9);
+		double v1=c1.potentialAt(x, y);
+		double v2=c2.potentialAt(x, y);
+		StdOut.printf("%.2e\n",  (v1 + v2));
+	}
 }
